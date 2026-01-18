77755614c42af8866ba316cf9cfc1f52ce3e8077
OM DHAGE <omdhage04@gmail.com>
2026-01-18 08:39:16 +0000

    diffrent variables declared in in e file

---
+++ Manage_variables.java
@@ -0,0 +1,11 @@
+public class Manage_variables{
+    public static void main(String[] args){
+        String name = "Dhage Om Santosh";
+        int age = 21;
+        float persentage = 35.60f;
+        short rollno = 45;
+        System.out.println("Name of the student is : "+name+"\nage is : "+age+"\nIts roll no is : "+rollno);
+    }
+
+
+}
