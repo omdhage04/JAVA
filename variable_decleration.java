d758ff2363764bdaf99aaacf1a0572e7a844314b
OM DHAGE <omdhage04@gmail.com>
2026-01-18 08:17:17 +0000

    Learn how to declare primitve data types in the java

---
+++ variable_decleration.java
@@ -0,0 +1,49 @@
+public class variable_decleration{
+    public static void main (String[] args){
+/* Intiger decleretion 
+        int x = 1212;
+        int y = 1000;          
+        System.out.println(x+y);
+*/
+/*  Character Decleretion
+    char its_o = 'O';
+    System.out.println(its_o);
+    }
+
+*/
+   
+/* byte Decleretion ( numaric value from -128 to 127)
+
+    byte B = 1000;
+    System.out.println(B);
+    
+*/
+/*
+   // short Decleretion ( Larger Numaric values then byte -32768 to 32767)
+    short l = 32000;
+    System.out.println(l);
+
+*/
+   /*
+    // long Decleretion 
+    long L = 100000000000L;
+    System.out.println(L);
+    */
+
+/*
+
+   float F = 32.123456789f;
+   System.out.println(F);
+
+   double D = 32.1234567891011121314151617181920;
+   System.out.println(D);
+
+
+*/
+boolean isitJAVA = true;
+System.out.println(isitJAVA);
+
+
+
+}
+}
