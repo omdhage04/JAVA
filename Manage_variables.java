18e0aa59fbb7aed2ea06820c49efe5a123badc18
OM DHAGE <omdhage04@gmail.com>
2026-01-19 17:39:33 +0000

    finalize the code

--- Manage_variables.java
+++ Manage_variables.java
@@ -4,7 +4,7 @@ public class Manage_variables{
         int age = 21;
         float persentage = 35.60f;
         short rollno = 45;
-        System.out.println("Name of the student is : "+name+"\nage is : "+age+"\nIts roll no is : "+rollno);
+        System.out.println("Name of the student is : "+name+"\nage is : "+age+"\nIts roll no is : "+rollno+"and it have percentage of "+persentage);
     }
 
 
