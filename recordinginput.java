cf247282fcf44236aa793725ffc60e72004817bd
OM DHAGE <omdhage04@gmail.com>
2026-01-20 16:43:21 +0000

    lern about taking user input with the help of Scanner class

---
+++ recordinginput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class recordinginput {
+    public static void main (String[] args){
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Enter any number ");
+        int num = ip.nextInt();
+        System.out.println(num);
+    }
+    
+}
