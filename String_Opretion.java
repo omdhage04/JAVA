6364d3e5b87dd87223aba52b06d4bf8817d583d5
OM DHAGE <omdhage04@gmail.com>
2026-01-22 16:11:07 +0000

    staging commit

---
+++ String_Opretion.java
@@ -0,0 +1,11 @@
+public class String_Opretion{
+
+    public static void main (String[] args){
+        String name = "My name is om santosh dhage +";
+        //String dom = " no i am not just kidding";
+        //int len = name.length();
+        //String concatresult = name.concat(dom);
+        String upper = name.toLowerCase();
+        System.out.println(upper);
+    }
+}
