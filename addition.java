9d14463fc807b789d28302989b95d2d0f5d85c5c
OM DHAGE <omdhage04@gmail.com>
2026-01-23 18:02:43 +0000

    lern how to initialize the array

--- addition.java
+++ addition.java
@@ -1,6 +1,18 @@
 public class addition {
     public static void main (String[] args){
         System.out.print("its printed");
+                System.out.print("its printed");
+
+                        System.out.print("its printed");
+
+                                System.out.print("its printed");
+
+                                        System.out.print("its printed");
+
+                                                System.out.print("its printed");
+
+                                                        System.out.print("its printed");
+
 
     }
 }
---
+++ array.java
@@ -0,0 +1,17 @@
+public class array {
+    public static void main (String[] args){
+        String subjects[];
+        subjects = new String [5];
+        subjects[0] = "Marathi";
+        subjects[1] = "Hindi";
+        subjects[2] = "English";
+        subjects[3] = "Science";
+        subjects[4] = "Mathematics";
+        System.out.println(subjects[4]);
+
+
+
+    }
+
+
+}
