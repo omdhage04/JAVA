34147b7b50c4903d28d0a527418d349ac37c60ae
OM DHAGE <omdhage04@gmail.com>
2026-01-24 16:17:26 +0000

    solve some of tasks only in if statement

--- onlyIF.java
+++ onlyIF.java
@@ -1,3 +1,5 @@
+/*
+creating a teenager chaking system
 import java.util.Scanner;
 public class onlyIF{
         public static void main (String[] args){
@@ -9,11 +11,74 @@ public class onlyIF{
                 System.out.println("You are just a teenagaeer");
                 
             }
-
-          
+            
+            System.out.println("your welcome , your age is"+num)
 }
 
 }
 
+*/
+/* 
+
+import java.util.Scanner;
+public class onlyIF{
+        public static void main (String[] args){
+            int num;
+            Scanner ip = new Scanner(System.in);
+            System.out.println("Enter Your age");
+            num = ip.nextInt();
+            if (num == 0){
+                System.out.println("really ");
+            }
+            if (num<18 && num > 0) {
+                System.out.println("You are just a teenagaeer");
+                
+            }
+            if(num>=18){
+            
+                System.out.println("your welcome , your age is"+num);
+            }
+}
+
+}
+*/
+
+/*
+
+//  level 1 : Even or odd Number
+import java.util.Scanner;
+public class onlyIF{
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Enter the number to cheack its Even or Odd ");
+        int num = ip.nextInt();
+        if(num%2==0){
+            System.out.println("Its an Even Number");
+        }
+        if(num%2!=0){
+            System.out.println("Its an Odd Number");
+        }
+    }
+}  
+*/
+
+// Level 2 : Find the largest number after taking the user input
+import java.util.Scanner;
+public class onlyIF{
+    public static void main (String[] args){
+        System.out.println("Lets cheack which is the largest Number");
+        System.out.println("Enter the First Number");
+        Scanner ip = new Scanner(System.in);
+        int num1 = ip.nextInt();
+        System.out.println("Enter the Second Number");
+        int num2 = ip.nextInt();
+        if (num1>num2){
+            System.out.print(num1+"is the Largest number");
+        }
+        if (num1<num2){
+            System.out.print(num2+"\nis the Largest number");
+        }
 
 
+    }
+}
