606b5324ae9438a8e0216e71800ed073b1899d0a
OM DHAGE <omdhage04@gmail.com>
2026-01-24 15:46:21 +0000

    lern the working of the only - if statement via exampeles

---
+++ onlyIF.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class onlyIF{
+        public static void main (String[] args){
+            int num;
+            Scanner ip = new Scanner(System.in);
+            System.out.println("Enter Your age");
+            num = ip.nextInt();
+            if (num<18) {
+                System.out.println("You are just a teenagaeer");
+                
+            }
+
+          
+}
+
+}
+
+
+
