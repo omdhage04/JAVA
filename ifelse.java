c03cd564c3bf29db12d1bb98e78b5a6b0947328b
OM DHAGE <omdhage04@gmail.com>
2026-01-25 16:04:51 +0000

    solve other problems to improve the if - elseif logic!

--- ifelse.java
+++ ifelse.java
@@ -64,7 +64,7 @@ public class ifelse{
 }
 
 
-*/
+
 import java.util.Scanner;
 public class ifelse{
     public static void main(String[] args) {
@@ -86,5 +86,101 @@ public class ifelse{
         }
     }
 
+//5️⃣ Pass or Fail
+
+//Input marks
+
+// 40 → Pass
+
+// 40 → Fail
 
 
+
+import java.util.Scanner;
+public class ifelse{
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        System.out.print("Enter your makes of Mathematics");
+        int math = ip.nextInt();
+        System.out.print("Enter your makes of Science");
+        int science = ip.nextInt();
+        System.out.print("Enter your makes of Physics");
+        int phy = ip.nextInt();
+        System.out.print("Enter your makes of English");
+        int eng = ip.nextInt();
+        System.out.print("Enter your makes of So-Science");
+        int So = ip.nextInt();
+        float result = (math+science+phy+eng+So)/5;
+        if (result >= 40){
+            System.out.println("you have "+result+"and you are pass");
+        }else{
+            System.out.println("you have "+result+"and you are Fail");
+        }
+
+    }
+}
+
+
+
+
+
+
+//login task
+import java.util.Scanner;
+public class ifelse{
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Enter your ID to log in ");
+        int id = ip.nextInt();
+        System.out.println("Enter your Pass to log in ");
+        int pa = ip.nextInt();
+        if(id == 7878 && pa == 7872 ){
+            System.out.println("Login Successful");
+        }else{
+            System.out.println("Faild invalid ID or Pass");
+        }
+
+
+
+
+    }
+}
+     */
+
+
+/*7️⃣ Grade Calculator
+
+90–100 → A
+
+75–89 → B
+
+60–74 → C
+
+< 60 → Fail */
+import java.util.Scanner;
+public class ifelse{
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Enter your Percentage \n");
+        float per = ip.nextFloat();
+        if(per < 0){
+            System.out.println("Enter a Valid Percentage ");
+
+        }else if(per < 60){
+            System.out.println("you are Fail");
+
+        }else if(per > 60 && per <74 ){
+            System.out.println("You got C grade ");
+
+        }else if(per > 75 && per  <89 ){
+            System.out.println("you Got B grade");
+
+        }else if(per >90 && per <= 100){
+            System.out.println("you Got A grade");
+
+        }else if(per > 100){
+            System.out.println("Enter a Valid Grade");
+
+        }
+    }
+}
