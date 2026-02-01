12327c7e135b1fc36e6079a3bdcc44999c51e204
OM DHAGE <omdhage04@gmail.com>
2026-02-01 16:30:05 +0000

    lerned how to print diffrent * patterns

--- printing.java
+++ printing.java
@@ -1,18 +1,66 @@
-import java.util.Scanner;
+/*
 
-public class printing {
+*
+**
+***
+****
+*****
+
+*/
+/*
+
+public class printing{
     public static void main(String[] args) {
-        System.out.println("Enter a number to print the pattern");
-        Scanner ip = new Scanner(System.in);
-        int Ii = ip.nextInt();        // Outer loop runs 5 times
-        for ( int i = 1; i <= Ii; i++) {
-            // Inner loop: Fixed the brackets and condition
-            for (int j = 1; j <= i; j++) { 
-                System.out.print(j + " ");
+        int a = 5;
+        for (int i = 1 ; i < a ; i ++){
+            for (int j = 1 ; j<=i;j++){
+                System.out.print('*');
             }
-            // Move to a new line after each row
-            System.out.println();
+        System.out.println();
         }
-        ip.close();
+    }
+}
+
+
+*/
+
+/*
+*****
+*****
+*****
+*****
+
+*/
+/* 
+public class printing{
+    public static void main(String[] args) {
+        int a = 4;
+        for (int i = 1 ; i <= a ; i ++){
+            for (int j = 1 ; j<=5;j++){
+                System.out.print('*');
+            }
+        System.out.println();
+        }
+    }
+}
+    */
+
+/*
+*****
+****
+***
+**
+*
+
+*/
+public class printing{
+    public static void main(String[] args) {
+        int a = 5;
+        for (int i = 1 ; i <= a ; i ++){
+            for (int j = a ; j>=i;j--){
+                System.out.print('*');
+            }
+        System.out.println();
+        }
     }
 }
